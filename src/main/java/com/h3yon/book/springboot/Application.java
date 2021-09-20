1b9d66997ea9509f00faeea9d5885efa2a0b0095
h3yon <khykhy1006@gmail.com>
2021-09-20 14:32:01 +0900

    테스트코드

---
+++ src/main/java/com/h3yon/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.h3yon.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
